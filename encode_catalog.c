4947daa8e2cd88d65d8b30ec9e6a648870c7e69b
0913vision <0913vision@gmail.com>
2024-05-17 11:13:35 +0900

    catalog_yc #19

--- encode_catalog.c
+++ encode_catalog.c
@@ -39,6 +39,85 @@ void process_directory(const char* directory, const char* catalog_path, FILE* f)
     char abs_path[PATH_MAX];
     char buffer[2048];
 
+    // 1. the directory itself
+    if (realpath(directory, abs_path) == NULL) {
+        perror("realpath");
+        return;
+    }
+
+    if (lstat(abs_path, &lstatbuf) == -1) {
+        perror("lstat");
+        return;
+    }
+
+    encode_stat_to_string(buffer, sizeof(buffer), abs_path, &lstatbuf);
+    fprintf(f, "lstat\n%s", buffer);
+
+    if (S_ISLNK(lstatbuf.st_mode)) {
+        if (stat(abs_path, &statbuf) == -1) {
+            fprintf(f, "stat\nNone\n");
+        } else {
+            encode_stat_to_string(buffer, sizeof(buffer), abs_path, &statbuf);
+            fprintf(f, "stat\n%s", buffer);
+        }
+    } else {
+        fprintf(f, "stat\n%s", buffer);
+    }
+
+    fprintf(f, "layout\n");
+    if (!S_ISREG(lstatbuf.st_mode)) {
+        fprintf(f, "None\n");
+    } else {
+        struct llapi_layout *layout;
+        int rc[5];
+        uint64_t count = 0, size, start, end, idx, interval, file_size;
+        layout = llapi_layout_get_by_path(abs_path, 0);
+        if (layout == NULL) {
+            printf("errno: %d\n", errno);
+            return;
+        }
+
+        file_size = lstatbuf.st_size;
+        rc[0] = llapi_layout_comp_use(layout, 1);
+        if (rc[0]) {
+            printf("error: layout component iteration failed\n");
+            llapi_layout_free(layout);
+            return;
+        }
+
+        while (1) {
+            rc[0] = llapi_layout_stripe_count_get(layout, &count);
+            rc[1] = llapi_layout_stripe_size_get(layout, &size);
+            rc[2] = llapi_layout_comp_extent_get(layout, &start, &end);
+            if (rc[0] || rc[1] || rc[2]) {
+                printf("error: cannot get stripe information\n");
+                llapi_layout_free(layout);
+                return;
+            }
+
+            interval = count * size;
+            end = (end < file_size) ? end : file_size;
+            int i;
+            for (i = 0; i < count; i++) {
+                rc[0] = llapi_layout_ost_index_get(layout, i, &idx);
+                if (rc[0]) {
+                    goto here_exit;
+                }
+                fprintf(f, "%s %lu %lu %lu %lu %lu %lu\n", abs_path, idx, start + i * size, end, interval, size, file_size);
+            }
+            rc[0] = llapi_layout_comp_use(layout, 3);
+            if (rc[0] == 0)
+                continue;
+            if (rc[0] < 0)
+                printf("error: layout component iteration failed\n");
+            break;
+        }
+        here_exit:
+        llapi_layout_free(layout);
+    }
+    fprintf(f, "end\n");
+    
+    // 2. subdir and file
     dir = opendir(directory);
     if (dir == NULL) {
         perror("opendir");
